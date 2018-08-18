217f3ab1961cb4cdd49e8b4654778b12f4302846
Mario Andrés Ruiz <marioarb.97@gmail.com>
2018-08-17 19:34:32 -0500

    ultimos cambios agosto 17

--- src/app/components/parte/parte-detail/parte-detail.component.ts
+++ src/app/components/parte/parte-detail/parte-detail.component.ts
@@ -26,20 +26,20 @@ import { UsuarioService } from '../../../services/usuario.service';
   styles: []
 })
 export class ParteDetailComponent implements OnInit {
-  infractor: Infractor;
+  
   estado: boolean;
   dependencia: number;
   tipoVehiculo: number;
   tipoComparendo: number;
   identificacion: number;
- // usuario: string;
+  usuario: number;
   parte = new Parte();
 
   constructor(
     private route: ActivatedRoute,
     public parteService: ParteService,
     public infractorService: InfractorService, 
-   // public usuarioService: UsuarioService, 
+    public usuarioService: UsuarioService, 
     public dependenciaService: DependenciaService,
     public tipoComparendoService: TipoComparendoService,
     public tipoVehiculoService: TipoVehiculoService,
@@ -51,6 +51,8 @@ export class ParteDetailComponent implements OnInit {
     this.dependenciaService.cargarDependencias().then(_ => this.dependencia = this.dependenciaService.dependencias[0].id);
     this.tipoComparendoService.cargarTipoComparendos().then(_ => this.tipoComparendo = this.tipoComparendoService.tipoComparendos[0].codigo);
     this.tipoVehiculoService.cargarTipoVehiculos().then(_ => this.tipoVehiculo = this.tipoComparendoService.tipoComparendos[0].codigo);
+    this.infractorService.cargarInfractores();
+    this.usuarioService.cargarUsuarios();
   }
 
   getParte() {
@@ -62,6 +64,8 @@ export class ParteDetailComponent implements OnInit {
         this.dependencia = this.parte.dependencia.id;
         this.tipoComparendo = this.parte.tipoComparendo.codigo;
         this.tipoVehiculo = this.parte.tipoVehiculo.codigo;
+
+      
         this.alertService.refresh();
       });
     } else {
@@ -69,24 +73,15 @@ export class ParteDetailComponent implements OnInit {
     }
   }
 
-  getInfractor() {
-      this.infractorService.cargarInfractor(this.identificacion).then((item: Infractor) => {
-        this.infractor = item;
-        this.infractor.tipoDocumento.codigo = this.infractor.tipoDocumento.codigo;
-        this.infractor.grado.codigo = this.infractor.grado.codigo;
-        this.alertService.refresh();
-      });
-      return this.infractor;
-    } 
   
 
   saveParte() {
-    debugger;
-    
     this.parte.dependencia = this.dependenciaService.dependencias.filter((item : Dependencia) => item.id === +this.dependencia)[0];
     this.parte.tipoComparendo = this.tipoComparendoService.tipoComparendos.filter((item : TipoComparendo) => item.codigo === +this.tipoComparendo)[0];
     this.parte.tipoVehiculo = this.tipoVehiculoService.tipoVehiculos.filter((item : TipoVehiculo) => item.codigo === +this.tipoVehiculo)[0];
-    this.parte.infractor = this.getInfractor();
+    this.parte.infractor = this.infractorService.infractores.filter((item : Infractor) => item.identificacion === +this.identificacion)[0];
+    this.parte.usuario = this.usuarioService.usuarios.filter((item : Usuario) => item.identificacion === +this.usuario)[0];
+    debugger;
     this.parteService.guardarParte(this.parte);
   }
 
